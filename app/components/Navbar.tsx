3a73914832d4546a754aa16b155326d0800271b3
anantariskys <gamersindo555@gmail.com>
2024-11-11 21:01:17 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import Logo from '~/assets/Logo.png'
 const Navbar = () => {
   return (
-    <nav className='w-full py-4'>
+    <nav className='w-full py-4 fixed top-0 bg-white'>
         <main className='container flex justify-between items-center '>
-            <img src={Logo} className='w-44 fixed top-0' alt="" />
+            <img src={Logo} className='w-44 ' alt="" />
             <div className='flex items-center gap-4'>
                 <p>test</p>
                 <p>test</p>
