f54ffd6f15bbe82c3934accffef275116dc44b25
anantariskys <gamersindo555@gmail.com>
2024-11-12 21:27:48 +0700

    Update IsuCard.tsx

--- app/components/IsuCard.tsx
+++ app/components/IsuCard.tsx
@@ -9,13 +9,16 @@ interface cardProps{
 
 const IsuCard:FC<cardProps> = ({description,title}) => {
   return (
-    <div className="rounded-xl p-4 hover:shadow-lg space-y-2 duration-300 hover:-translate-y-1 transition-transform bg-white border-2">
-      <h5 className="text-2xl md:text-4xl font-extrabold">{title}</h5>
-      <p>{description}</p>
+    <div className="rounded-xl px-4 py-12 hover:shadow-lg space-y-2 duration-300 hover:-translate-y-1 flex flex-col justify-between transition-transform bg-white border-2">
+      <h5 className="text-2xl  md:text-4xl font-extrabold">{title}</h5>
+      <div className="space-y-4">
+      <p className="text-center">{description}</p>
 
-      <small className="flex items-center gap-1 text-primary">
+      <small className="flex justify-center items-center gap-1 text-primary">
         Mulai diskusi <Icon icon={"ic:round-play-arrow"} />
       </small>
+
+      </div>
     </div>
   );
 };
