3746a5271ff2e17a8b13556502b8b518ea367d92
anantariskys <gamersindo555@gmail.com>
2024-11-12 08:58:14 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,51 @@
-import Logo from '~/assets/Logo.png'
-const Navbar = () => {
-  return (
-    <nav className='w-full py-4 fixed top-0 bg-white'>
-        <main className='container flex justify-between items-center '>
-            <img src={Logo} className='w-44 ' alt="" />
-            <div className='flex items-center gap-4'>
-                <p>test</p>
-                <p>test</p>
-                <p>test</p>
+import Logo from "~/assets/Logo.png";
+import Dropdown from "./Dropdown";
+import Button from "./Button";
+import { Form, Link } from "@remix-run/react";
+import { FC } from "react";
+import { User } from "@supabase/supabase-js";
 
-            </div>
-
-        </main>
-      
-    </nav>
-  )
+interface NavbarProps {
+  user: User;
 }
+const Navbar: FC<NavbarProps> = ({ user }) => {
+  return (
+    <nav className="w-full py-4 fixed top-0 bg-white">
+      <main className="container flex justify-between items-center ">
+        <Link to={"/"}>
+          <img src={Logo} className="w-44 " alt="" />
+        </Link>
+        <div className="flex items-center justify-between gap-4 cursor-pointer">
+          <Dropdown title="Isu">
+            <p className="py-1">Lingkungan</p>
+            <p className="py-1">Kebijakan</p>
+            <p className="py-1">Ham</p>
+            <p className="py-1">Pembangunan</p>
+          </Dropdown>
+          <p>Tentang Kami</p>
+          {user ? (
+            <Dropdown title={user.email}>
+              <p className="py-2">profile</p>
+              <Form method="post" action="/api/logout">
+                <Button width="w-fit">Keluar</Button>
+              </Form>
+            </Dropdown>
+          ) : (
+            <div className="flex items-center gap-4">
+              <Link to={"/register"}>
+                <Button width="w-fit" variant="primary-outline">
+                  Daftar
+                </Button>
+              </Link>
+              <Link to={"/login"}>
+                <Button width="w-fit">Masuk</Button>
+              </Link>
+            </div>
+          )}
+        </div>
+      </main>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
