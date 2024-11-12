628ce57e4544a2d9d58eef35a0703ee9da743218
anantariskys <gamersindo555@gmail.com>
2024-11-12 08:58:25 +0700

    Create api.logout.tsx

---
+++ app/routes/api.logout.tsx
@@ -0,0 +1,13 @@
+import { ActionFunction, redirect } from "@remix-run/node"
+import { supabase } from "~/utils/supabaseClient"
+
+export const action: ActionFunction = async ({ request }) => {
+  const logout = await supabase.auth.signOut()
+
+  if (logout.error) {
+    return new Response(logout.error.message, { status: 400 })
+  } 
+
+  return redirect("/login")
+  
+}
