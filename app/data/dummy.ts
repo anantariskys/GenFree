01629b6ad1095c7682ea9c5d53da946920f06dd9
anantariskys <gamersindo555@gmail.com>
2024-11-12 20:47:03 +0700

    Create dummy.ts

---
+++ app/data/dummy.ts
@@ -0,0 +1,26 @@
+export const isuData =[
+    {
+        id:1,
+        title:"Isu 🌏 Lingkungan",
+        description : 'Selamatkan Alam Kita',
+        url:""
+    },
+    {
+        id:2,
+        title:"Isu ⚖ Kebijakan",
+        description : 'Kebijakan Untuk Rakyat',
+        url:""
+    },
+    {
+        id:3,
+        title:"Isu ✊ HAM",
+        description : 'Hormati Hak Asasi',
+        url:""
+    },
+    {
+        id:4,
+        title:"Isu 🏗️ Pembangunan",
+        description : 'Pembangunan Berbasis Keadilan',
+        url:""
+    },
+]
