38ff89337b5cc2ce0cba17d368602583f253b730
anantariskys <gamersindo555@gmail.com>
2024-11-12 20:46:46 +0700

    Update AuthLayout.tsx

--- app/components/auth/AuthLayout.tsx
+++ app/components/auth/AuthLayout.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react";
 import React, { FC } from "react";
 
 import Logo from '~/assets/Logo.png'
@@ -6,7 +7,9 @@ const AuthLayout:FC<{children:React.ReactNode}> = ({children}) => {
   return (
     <main className="container flex items-center justify-center gap-16 h-screen md:p-10">
       <div className=" md:max-w-lg  w-full relative h-full flex md:flex-row flex-col items-center justify-center  md:p-8">
+        <Link to={'/'}>
         <img src={Logo} className="w-44 relative md:absolute md:top-0 left-0 md:left-8" alt="" />
+        </Link>
         {children}
       </div>
       <div className="h-full  aspect-square hidden md:flex items-center justify-center  rounded-2xl">
