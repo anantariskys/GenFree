a45f8635a21d2ab473e7e676e9f68ca97d2b553e
anantariskys <gamersindo555@gmail.com>
2024-11-18 09:21:36 +0700

    Update IsuInformation.tsx

--- app/components/IsuInformation.tsx
+++ app/components/IsuInformation.tsx
@@ -7,8 +7,8 @@ interface Props{
 const IsuInformation:FC<Props> = ({children,title}) => {
   return (
     <div className="max-w-4xl mx-auto space-y-2 bg-gray-100 rounded-2xl p-8 shadow-xl  border w-full">
-      <h2 className="text-4xl font-bold text-secondary">{title}</h2>
-      <p>
+      <h2 className="md:text-4xl text-2xl font-bold text-secondary">{title}</h2>
+      <p className="text-sm md:text-base">
         {children}
       </p>
     </div>
