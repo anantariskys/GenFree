8733da6c3bfd89446cfa2bbde388509bb712e360
anantariskys <gamersindo555@gmail.com>
2024-11-21 21:19:15 +0700

    Update admin.tsx

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,10 +1,9 @@
 import { json, LoaderFunction, redirect } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { c } from "node_modules/vite/dist/node/types.d-aGj9QkWt";
 import AdminLayout from "~/components/page/AdminLayout";
 import { sessionStorage } from "~/utils/session";
 import { supabase } from "~/utils/supabaseClient";
-
-
 
 export const loader: LoaderFunction = async ({ params, request }) => {
   const session = await sessionStorage.getSession(
@@ -15,18 +14,21 @@ export const loader: LoaderFunction = async ({ params, request }) => {
   if (!userId) {
     redirect("/login");
   }
-  console.log(userId)
-  const isAdmin = await supabase.from("profiles").select("*").eq("user_id", userId).single();
-  console.log(isAdmin.data.role)
+  const isAdmin = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("user_id", userId)
+    .single();
+
   if (!isAdmin.data) {
-    return redirect("/login");  
+    return redirect("/login");
   }
   if (isAdmin.data.role === false) {
-  return redirect('/')
+    return redirect("/");
   }
 
   return json({ user: isAdmin.data }, { status: 200 });
-}
+};
 export default function Admin() {
   return (
     <AdminLayout>
