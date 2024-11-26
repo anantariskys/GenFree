a5036555d81ebd42dba38a92b84ed710adaf3c41
anantariskys <gamersindo555@gmail.com>
2024-11-26 21:47:55 +0700

    Update dummy.ts

--- app/data/dummy.ts
+++ app/data/dummy.ts
@@ -32,27 +32,84 @@ export const whyData = [
       icon: "mingcute:keyhole-fill",
       title: "Ruang Aman",
       description:
-        "Menyediakan ruang aman bagi Gen-Z sehingga tidak perlu takut mendapatkan ancaman ketika mengutarakan opini.",
+        "Gen-Free menyediakan ruang aman yang inklusif bagi Gen-Z untuk mengutarakan opini.",
     },
     {
       id: 2,
-      icon: "mingcute:user-4-fill",
-      title: "Anomalitas",
-      description: "Gen-Free menekankan prinsip anonimitas.",
+      icon: "mdi:check-circle",
+      title: "Akses Mudah",
+      description: " Gen-Free mudah diakses oleh Gen-Z selaku target penggunanya dimanapun dan kapanpun.",
     },
     {
       id: 3,
-      icon: "mdi:check-circle",
-      title: "Akses Mudah",
+      icon: "mingcute:user-4-fill",
+      title: "Anonimitas",
       description:
-        "Gen-Free mudah diakses oleh Gen-Z selaku target penggunanya dimanapun dan kapanpun.",
+        "Gen-Free menekankan prinsip anonimitas untuk menghindari subjektivitas argumentasi dari pengguna, serta menghindari adanya bias dan stigma negatif dari opini yang diutarakan.",
     },
     {
       id: 4,
       icon: "tabler:filter",
-      title: "Filter Kata",
+      title: "Fact Checking dan Filter Kata",
       description:
-        "Gen-Free menyediakan sistem report dan filter kata, sehingga berpotensi meminimalisir adanya perselisihan.",
+        "Gen-free menyediakan sistem filter kata dan fact-checking untuk memastikan bahwa diskusi yang dihasilkan tetap berada dalam koridor demokrasi, serta menghindari konflik antar-pengguna.",
     },
   ];
+  
+
+  export const teamData = [
+    {
+      name: "Bunga Zahra Lianti Sudibyo",
+      role: "Frontend Developer",
+      img: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+    {
+      name: "Nadia Sartika Stephanie Manalu",
+      role: "Frontend Developer",
+      img: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+    {
+      name: "Muhammad Nurfarhan",
+      role: "Backend Developer",
+      img: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+    {
+      name: "Kezia Varda Yanita",
+      role: "UI/UX Designer",
+      img: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+    {
+      name: "Alda Gracia Winaryono",
+      role: "Quality Assurance",
+      img: "https://xsgames.co/randomusers/avatar.php?g=female",
+    },
+    {
+      name: "Divan Farrel Amanna",
+      role: "Project Manager",
+      img: "https://xsgames.co/randomusers/avatar.php?g=male",
+    },
+  ];
+
+
+ export const howToUseData  = [
+    {
+      id: 1,
+      title: "Pilih Isu yang Relevan denganmu!",
+      description:
+        "Temukan mosi diskusi yang sesuai dengan minat, keahlian, atau kebutuhanmu. Kamu bisa memilih isu lingkungan, kebijakan, HAM, pembangunan, maupun isu gender.",
+    },
+    {
+      id: 2,
+      title: "Suarakan pendapatmu!",
+      description:
+        "Berikan argumentasimu terhadap studi kasus yang disediakan. Sertakan bukti konkret yang berasal dari sumber kredibel untuk memperkuat opinimu.",
+    },
+    {
+      id: 3,
+      title: "Terlibatlah dalam diskusi!",
+      description:
+        "Deliberasi adalah kunci dari tercapainya demokrasi! Maka dari itu, pastikan kamu terlibat aktif dalam diskusi dengan pengguna lainnya.",
+    },
+  ];
+  
   
