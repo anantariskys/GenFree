723216e941f4a81e3e36b792dca1349ea026e75c
anantariskys <gamersindo555@gmail.com>
2024-11-26 21:47:52 +0700

    Update PageLayout.tsx

--- app/components/page/PageLayout.tsx
+++ app/components/page/PageLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 import Footer from "../Footer";
 import { User } from "@supabase/supabase-js";
 
-const PageLayout: React.FC<{ children: React.ReactNode,isu:{name:string,slug:string}[] ,user:{name:string},variant?:boolean}> = ({ children,user,variant ,isu}) => {
+const PageLayout: React.FC<{ children: React.ReactNode,isu:{name:string,slug:string}[] ,user:{name:string,role:boolean},variant?:boolean}> = ({ children,user,variant ,isu}) => {
   return (
     <>
       <Navbar  user={user} isu={isu}  />
