357a84164d51010ffb42c3ad67fd4939c661c929
anantariskys <gamersindo555@gmail.com>
2024-11-26 21:48:06 +0700

    Update admin._index.tsx

--- app/routes/admin._index.tsx
+++ app/routes/admin._index.tsx
@@ -1,5 +1,11 @@
+import { LoaderFunction } from "@remix-run/node";
 import React from "react";
 
+
+export const loader: LoaderFunction = async ({ params, request }) => {
+  
+  return null
+}
 export default function AdminDashboard() {
   return <h1>Admin Dashboard</h1>;
 }
