f04895fc35ff57d947266e547b6d9511f12a4197
anantariskys <gamersindo555@gmail.com>
2024-12-04 06:27:05 +0700

    Update dummy.ts

--- app/data/dummy.ts
+++ app/data/dummy.ts
@@ -1,32 +1,69 @@
-export const isuData =[
-    {
-        id:1,
-        title:"Isu 🌏 Lingkungan",
-        description : 'Selamatkan Alam Kita',
-        url:""
-    },
-    {
-        id:2,
-        title:"Isu ⚖ Kebijakan",
-        description : 'Kebijakan Untuk Rakyat',
-        url:""
-    },
-    {
-        id:3,
-        title:"Isu ✊ HAM",
-        description : 'Hormati Hak Asasi',
-        url:""
-    },
-    {
-        id:4,
-        title:"Isu 🏗️ Pembangunan",
-        description : 'Pembangunan Berbasis Keadilan',
-        url:""
-    },
-]
+import Rara from '~/assets/rara.jpg'
+import Farhan from '~/assets/Farhan.jpg'
+import Kezia from '~/assets/Kezie.jpg'
+import Alda from '~/assets/alda.jpg'
+import Divan from '~/assets/divan.jpg'
+import Nadia from '~/assets/Nadia.jpg'
 
 
-export const whyData = [
+
+  
+
+  export const teamData = [
+    {
+      name: "Bunga Zahra Lianti Sudibyo",
+      role: "Frontend Developer",
+      img: Rara,
+    },
+    {
+      name: "Nadia Sartika Stephanie Manalu",
+      role: "Frontend Developer",
+      img: Nadia,
+    },
+    {
+      name: "Muhammad Nurfarhan",
+      role: "Backend Developer",
+      img: Farhan,
+    },
+    {
+      name: "Kezia Varda Yanita",
+      role: "UI/UX Designer",
+      img: Kezia,
+    },
+    {
+      name: "Alda Gracia Winaryono",
+      role: "Quality Assurance",
+      img: Alda,
+    },
+    {
+      name: "Divan Farrel Amanna",
+      role: "Project Manager",
+      img: Divan,
+    },
+  ];
+
+
+ export const howToUseData  = [
+    {
+      id: 1,
+      title: "Pilih Isu yang Relevan denganmu!",
+      description:
+        "Temukan mosi diskusi yang sesuai dengan minat, keahlian, atau kebutuhanmu. Kamu bisa memilih isu lingkungan, kebijakan, HAM, pembangunan, maupun isu gender.",
+    },
+    {
+      id: 2,
+      title: "Suarakan pendapatmu!",
+      description:
+        "Berikan argumentasimu terhadap studi kasus yang disediakan. Sertakan bukti konkret yang berasal dari sumber kredibel untuk memperkuat opinimu.",
+    },
+    {
+      id: 3,
+      title: "Terlibatlah dalam diskusi!",
+      description:
+        "Deliberasi adalah kunci dari tercapainya demokrasi! Maka dari itu, pastikan kamu terlibat aktif dalam diskusi dengan pengguna lainnya.",
+    },
+  ];
+  export const whyData = [
     {
       id: 1,
       icon: "mingcute:keyhole-fill",
@@ -55,61 +92,29 @@ export const whyData = [
         "Gen-free menyediakan sistem filter kata dan fact-checking untuk memastikan bahwa diskusi yang dihasilkan tetap berada dalam koridor demokrasi, serta menghindari konflik antar-pengguna.",
     },
   ];
-  
-
-  export const teamData = [
+  export const isuData =[
     {
-      name: "Bunga Zahra Lianti Sudibyo",
-      role: "Frontend Developer",
-      img: "https://xsgames.co/randomusers/avatar.php?g=male",
+        id:1,
+        title:"Isu 🌏 Lingkungan",
+        description : 'Selamatkan Alam Kita',
+        url:""
     },
     {
-      name: "Nadia Sartika Stephanie Manalu",
-      role: "Frontend Developer",
-      img: "https://xsgames.co/randomusers/avatar.php?g=female",
+        id:2,
+        title:"Isu ⚖ Kebijakan",
+        description : 'Kebijakan Untuk Rakyat',
+        url:""
     },
     {
-      name: "Muhammad Nurfarhan",
-      role: "Backend Developer",
-      img: "https://xsgames.co/randomusers/avatar.php?g=male",
+        id:3,
+        title:"Isu ✊ HAM",
+        description : 'Hormati Hak Asasi',
+        url:""
     },
     {
-      name: "Kezia Varda Yanita",
-      role: "UI/UX Designer",
-      img: "https://xsgames.co/randomusers/avatar.php?g=female",
+        id:4,
+        title:"Isu 🏗️ Pembangunan",
+        description : 'Pembangunan Berbasis Keadilan',
+        url:""
     },
-    {
-      name: "Alda Gracia Winaryono",
-      role: "Quality Assurance",
-      img: "https://xsgames.co/randomusers/avatar.php?g=female",
-    },
-    {
-      name: "Divan Farrel Amanna",
-      role: "Project Manager",
-      img: "https://xsgames.co/randomusers/avatar.php?g=male",
-    },
-  ];
-
-
- export const howToUseData  = [
-    {
-      id: 1,
-      title: "Pilih Isu yang Relevan denganmu!",
-      description:
-        "Temukan mosi diskusi yang sesuai dengan minat, keahlian, atau kebutuhanmu. Kamu bisa memilih isu lingkungan, kebijakan, HAM, pembangunan, maupun isu gender.",
-    },
-    {
-      id: 2,
-      title: "Suarakan pendapatmu!",
-      description:
-        "Berikan argumentasimu terhadap studi kasus yang disediakan. Sertakan bukti konkret yang berasal dari sumber kredibel untuk memperkuat opinimu.",
-    },
-    {
-      id: 3,
-      title: "Terlibatlah dalam diskusi!",
-      description:
-        "Deliberasi adalah kunci dari tercapainya demokrasi! Maka dari itu, pastikan kamu terlibat aktif dalam diskusi dengan pengguna lainnya.",
-    },
-  ];
-  
-  
+]
