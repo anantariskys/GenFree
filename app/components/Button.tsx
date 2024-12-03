59d89c97b192874efa884f4a8e7df7de693d4d3f
anantariskys <gamersindo555@gmail.com>
2024-12-04 06:26:35 +0700

    Update Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,7 +2,7 @@ import React, { ButtonHTMLAttributes, Children } from 'react'
 
 interface buttonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
    children : React.ReactNode,
-   variant? : 'primary'|'primary-outline'|'setuju'|'tolak'|'secondary'|'dark',
+   variant? : 'primary'|'primary-outline'|'setuju'|'tolak'|'secondary'|'dark'|'dark-outline',
    width? : 'w-full'|'w-fit',
   
 }
@@ -18,15 +18,18 @@ const Button:React.FC<buttonProps> = ({variant='primary',width='w-full',children
         case 'primary-outline':
             className='bg-transparent border  border-primary hover:bg-primary hover:text-white transition-colors text-primary'
             break;
-        case 'setuju':
-            className='bg-green-500 border   hover:bg-green-600 text-white transition-colors '
-            break;
-        case 'tolak':
-            className='bg-red-500 border   hover:border-red-600 text-white transition-colors '
-            break;
-        case 'dark':
-            className='bg-neutral-800 border   hover:border-neutral-900 text-white transition-colors '
-            break;
+            case 'setuju':
+                className='bg-green-500 border   hover:bg-green-600 text-white transition-colors '
+                break;
+                case 'tolak':
+                    className='bg-red-500 border   hover:border-red-600 text-white transition-colors '
+                    break;
+                    case 'dark':
+                        className='bg-neutral-800 border   hover:border-neutral-900 text-white transition-colors '
+                        break;
+                        case 'dark-outline':
+                            className='bg-transparent border  border-neutral-800 hover:bg-neutral-800 hover:text-white transition-colors text-neutral-800'
+                            break;
         default:
             break;
     }
